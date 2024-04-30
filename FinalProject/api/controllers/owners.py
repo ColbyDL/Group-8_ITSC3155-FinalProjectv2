2ad025e2ebb0c34b7aa2b842184f825686636e01
Colby Lalicker <colby.d.lalicker@gmail.com>
2024-04-30 14:43:54 -0400

    feat: constraint added to the review score

--- FinalProject/api/controllers/owners.py
+++ FinalProject/api/controllers/owners.py
@@ -1,10 +1,10 @@
 from sqlalchemy import func, and_, DECIMAL
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Response, Depends
 
-# from fastapi import HTTPException, status, Response, Depends
-# from ..models import orders as model
-# from sqlalchemy.exc import SQLAlchemyError
 from datetime import datetime
+
 
 from ..models.orders import Orders
 
--- FinalProject/api/routers/users.py
+++ FinalProject/api/routers/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, FastAPI, status, Response
+from fastapi import APIRouter, Depends, FastAPI, status, Response, HTTPException
 from sqlalchemy.orm import Session
 from ..controllers import users as controller
 from ..schemas import menu as schema
@@ -9,6 +9,4 @@ router = APIRouter(tags=["Users"], prefix="/users")
 
 @router.get("/{foodCategory}", response_model=list[schema.Menu])
 def read_all_categories(foodCategory: str, db: Session = Depends(get_db)):
-    if foodCategory:
-        return controller.read_all_categories(db=db, foodCategory=foodCategory)
-    return controller.read_all_categories(db=db)
+    return controller.read_all_categories(db, foodCategory=foodCategory)
